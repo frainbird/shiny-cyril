15b914a42019554f26870a675e8d0b57bea4bfdd
Michael John Valk <mjvalk@macl-04.cis.utas.edu.au>
2012-10-03 12:29:22 +1000

    Updated names implementation

--- Pig/NamesViewController.m
+++ Pig/NamesViewController.m
@@ -12,6 +12,11 @@
 
 @end
 //local variables
+NSString* P1Name = @"";
+NSString* P2Name = @"";
+
+static NSString *P1Key = @"player1Name";
+static NSString *P2Key = @"player2Name";
 
 
 @implementation NamesViewController
@@ -35,6 +40,8 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    [self readNames]; //get names from file
+    [self setNames];
 }
 
 - (void)viewDidUnload
@@ -49,9 +56,51 @@
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
+
+
+
 -(IBAction)acceptButtonPressed:(id)sender
 {
+    
+    [self writeNames];
     [self exitToMenu];
+}
+
+-(void)readNames
+{
+    //read in names
+    
+    NSString *name1 = [[NSUserDefaults standardUserDefaults] stringForKey:P1Key];
+    NSString *name2 = [[NSUserDefaults standardUserDefaults] stringForKey:P2Key];
+    
+    //store them in variables if something is in them
+    if (![name1 length] == 0)
+    {
+        P1Name = name1;
+    }
+    
+    if (![name2 length] == 0)
+    {
+        P2Name = name2;
+    }
+    
+}
+
+-(void)setNames
+{
+    P1NameField.text = P1Name;
+    P2NameField.text = P2Name;
+}
+
+-(void)writeNames
+{
+    //get values from name fields
+    P1Name = P1NameField.text;
+    P2Name = P2NameField.text;
+    
+    //write names to file
+    [[NSUserDefaults standardUserDefaults] setObject:P1Name forKey:P1Key];
+    [[NSUserDefaults standardUserDefaults] setObject:P2Name forKey:P2Key];
 }
 
 -(void)exitToMenu
